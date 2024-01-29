d7c2fb2c94a07d518258fbe43cd784941aa1c352
Gabi <gabrielajustinodesouza@gmail.com>
2024-01-28 23:05:05 -0300

    configurando big-card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuBarComponent } from "./component/menu-bar/menu-bar.component";
+import { BigCardComponent } from './component/big-card/big-card.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, BigCardComponent]
 })
 export class AppComponent {
   title = 'blog';
