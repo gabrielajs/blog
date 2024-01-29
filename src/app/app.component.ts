ceec6a38374a5430dcde38abc28027aefb20cd6f
Gabi <gabrielajustinodesouza@gmail.com>
2024-01-28 23:34:16 -0300

    criando componente pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MenuBarComponent } from "./component/menu-bar/menu-bar.component";
-import { BigCardComponent } from './component/big-card/big-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, BigCardComponent]
+    imports: [RouterOutlet,HomeComponent]
 })
 export class AppComponent {
   title = 'blog';
