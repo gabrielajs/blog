5b39e8fbbaad5982452f5b27ee1f4ee83852894b
Gabi <gabrielajustinodesouza@gmail.com>
2024-01-30 00:09:58 -0300

    configurando layout do menu

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { MenuTitleComponent } from "../../component/menu-title/menu-title.component";
 import { BigCardComponent } from '../../component/big-card/big-card.component';
 import { SmallCardComponent } from '../../component/small-card/small-card.component';
+import { MenuBarComponent } from '../../component/menu-bar/menu-bar.component';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [MenuTitleComponent, BigCardComponent, SmallCardComponent]
+    imports: [MenuTitleComponent, BigCardComponent, SmallCardComponent, MenuBarComponent]
 })
 export class HomeComponent {
 
