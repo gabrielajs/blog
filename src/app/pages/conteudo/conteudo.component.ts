6d58fe40ceb75c877d4883131846c9213aef5097
Gabi <gabrielajustinodesouza@gmail.com>
2024-01-31 10:13:48 -0300

    deixando pagina conteudo dinamico

--- src/app/pages/conteudo/conteudo.component.ts
+++ src/app/pages/conteudo/conteudo.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake';
 
 @Component({
   selector: 'app-conteudo',
@@ -8,5 +10,34 @@ import { Component } from '@angular/core';
   styleUrl: './conteudo.component.css'
 })
 export class ConteudoComponent {
+
+  photoCover:string = "";
+  contentTitle:string = "";
+  contentDescription:string = "";
+
+  private id:string | null = "0";
+
+  constructor(private route:ActivatedRoute){
+
+  }
+
+  ngOnInit():void{
+    this.route.paramMap.subscribe(
+      value => this.id = value.get("id")
+    )
+
+    this.setValuesToComponent(this.id!)
+  }
+
+  setValuesToComponent(id:string){
+    const result = dataFake.filter(
+      article => article.id == id
+    )[0]
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photo
+
+  }
 
 }
